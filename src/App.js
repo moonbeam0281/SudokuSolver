d7be3884b060fa2ee7d0fe6b42c10021ad4eed4e
moonbeam0281 <moonbeam32145@gmail.com>
2023-09-01 22:45:10 +0200

    Updating 1
    Updating and adding new form and fucntionallity

--- src/App.js
+++ src/App.js
@@ -4,28 +4,95 @@ import { useState } from "react";
 
 const InpBtn = stats =>
 {
-  const {name, index} = stats;
+  const {index, x, y} = stats;
   const [bVal, setVal] = useState(0);
+  const [isHovered, setHover] = useState(0);
+
+  const handleMouseEnter = () => {
+    inputField.filter(e => e.index == 6).forEach(e => {e.style = btnStyleHoverNear})
+    setHover(true);
+  };
+ const handleMouseLeave = () => {
+    setHover(false);
+  };
+
+  const btnStyle1 = {
+    width: "58px",
+    height: "58px",
+    backgroundColor: "rgb(187, 236, 255)",
+    textAlign: "center",
+    fontSize: "20px", 
+    margin: "1px 1px 1px 1px",
+    border:"none"
+
+  }
+  const btnStyle2 = {
+    width: "58px",
+    height: "58px",
+    backgroundColor: "rgb(255, 145, 209)",
+    textAlign: "center",
+    fontSize: "20px", 
+    margin: "1px 1px 1px 1px",
+    border:"none"
+
+  }
+  const btnStyleHover = {
+    width: "58px",
+    height: "58px",
+    backgroundColor: "rgb(152, 255, 67)",
+    textAlign: "center",
+    fontSize: "20px", 
+    margin: "1px 1px 1px 1px",
+    border:"none",
+    borderRadius: "15px"
+
+  }
+  const btnStyleHoverNear = {
+    width: "58px",
+    height: "58px",
+    backgroundColor: "rgb(67, 255, 195)",
+    textAlign: "center",
+    fontSize: "20px", 
+    margin: "1px 1px 1px 1px",
+    border:"none"
+
+  }
 
   const changeVal = () =>
   {
     setVal(prev => prev >= 9? 0:prev + 1);
   };
 
-  const getName = () => {
-    return name;
+  const getStyle = () =>
+  {
+    switch(index %2 === 0)
+    {
+      case true: return isHovered? btnStyleHover : btnStyle1;
+      case false: return isHovered? btnStyleHover : btnStyle2;
+      default: return btnStyle1; 
+    }
   }
 
   return (
     <button 
     className={index}
     onClick={changeVal}
-    >{bVal === 0? "" : bVal}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    style={getStyle()}
+    >{bVal === 0? '' : bVal}
     </button>
-  )
+  );
 }
 
 const inputField = [];
+
+const colorNearby = (arr) =>
+  {
+    arr.forEach(element => {
+      
+    });
+  }
 
 function createNewTable()
 {
@@ -33,7 +100,7 @@ function createNewTable()
   {
     for(var j = 1; j <= 9; j++)
     {
-      inputField.push("b-" + i + "-" + j)
+      inputField.push({x: i, y: j})
     }
   }
 }
@@ -48,9 +115,9 @@ function App() {
       </header>
       <div className="InputField">
         <div className="InputGrid">
-          {
-            inputField.map((x, y) => 
-              <InpBtn name = {x} key = {y} index ={y%2 === 0? "inputButtons-1": "inputButtons-2"}></InpBtn>
+          { 
+            inputField.map((elm, i) => 
+              <InpBtn x = {elm.x} y = {elm.y} key = {i} index = {i + 1}></InpBtn>
             )
           }
         </div>
